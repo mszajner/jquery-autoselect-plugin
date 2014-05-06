43f8731f2de36f14013ef463b906be95ec238de0
Mirosław Szajner <mszajner@rexoft.pl>
2014-05-06 14:17:19 +0200

    Restore valid content of file

--- jquery.autoselect.plugin.js
+++ jquery.autoselect.plugin.js
@@ -1,36 +1,11 @@
 (function($) {
 	
-	$.fn.autoselect = function (filterCallback) {
+	$.fn.autoselect = function () {
 		return this.each(function() {
-			$(this).keyup(function(ev){
-				var currentValue = filterCallback.apply(this);
-				$(this).data('currentValue', currentValue);
-				if (typeof this.innerText == 'undefined') {
-					$(this).val(currentValue);
-				} else {
-					var selectionStart = this.selectionStart;
-					var selectionEnd = this.selectionEnd;
-					try {
-						this.innerText = val;
-					} catch (e) {
-						this.value = val;
-					}
-					this.selectionStart = selectionStart;
-					this.selectionEnd = selectionEnd;
-				}
-			}).focusin(function() {
-				if ($.browser.safari) {
-					var previousValue = $(this).val();
-					$(this).data('previousValue', previousValue);
-					$(this).data('currentValue', previousValue);
-				}
-			}).focusout(function() {
-				if ($.browser.safari) {
-					var previousValue = $(this).data('previousValue');
-					var currentValue = $(this).data('currentValue');
-					if (previousValue != currentValue) {
-						$(this).trigger('change');
-					}
+			$(this).focus(function(ev) {
+				if (ev.isSimulated) {
+					var that = this;
+					setTimeout(function() { that.select(); }, 100);
 				}
 			});
 		});
